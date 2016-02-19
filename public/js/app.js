3853a7b24323de26bdac9b9b5b7c64685edfcd98
adaube <adam_daube@yahoo.com>
2016-02-18 20:43:21 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,66 @@
+var weatherData = {};
+
+$(document).ready(function(){
+  var baseUrl = 'https://api.forecast.io/forecast/';
+  var name = "Your Name";
+  // $('#get-weather').on('click', getWeather);
+  $('#get-weather').on('click', showInfo);
+
+
+  function buildUrl(lat, lon){
+    // return "https://api.forecast.io/forecast/099641b3c36867977fb67a197504e301/37.8267,-122.423"
+    return baseUrl + apiKey+'/'+lat+','+lon;
+  }
+
+  function getWeather(){
+    var lat = $('#latitude').val();
+    var lon = $('#longitude').val();
+    var options = {
+      url: buildUrl(lat, lon),
+      dataType: 'jsonp',
+      success: successHandler,
+      error: errorHandler
+    };
+
+    $.ajax(options);
+  }
+
+
+  function successHandler(data){
+    weatherData = data;
+    $('#output').text(JSON.stringify(data));
+    console.log(data);
+  }
+
+  function errorHandler(error){
+    console.log(error);
+  }
+  function showInfo(){
+    var lat = $('#latitude').val();
+    var lon = $('#longitude').val();
+    var ajaxOptions = {
+      url: buildUrl(lat,lon),
+      dataType: 'jsonp',
+      success: showInfoSuccess,
+      error: errorHandler
+    }
+
+    $.ajax(ajaxOptions);
+
+  };
+
+  function showInfoSuccess(hammer){
+    console.log(hammer);
+    var source = $('#info').html();
+    var template = Handlebars.compile(source);
+    var extractedData = {
+      latitude: hammer.latitude,
+      longitude: hammer.longitude,
+      icon: hammer.currently.icon || 'clear-night',
+      summary: hammer.currently.summary,
+      time: moment(hammer.currently.time).format('LLLL')
+    };
+    var html = template(extractedData);
+    $('#test-output').html(html);
+  }
+});
