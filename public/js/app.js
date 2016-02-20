8a1196097bef5f09da717a6ccad2209bc24123b6
adaube <adam_daube@yahoo.com>
2016-02-20 05:17:40 -0500

    tweaking js

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,48 @@
 var weatherData = {};
+var googleData = {};
 
 $(document).ready(function(){
   var baseUrl = 'https://api.forecast.io/forecast/';
-  var name = "Your Name";
+  var locationUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
   $('#get-weather').on('click', showInfo);
 
-  function google(){
-    var googleResults = $('#location').results[0].geometry.location(lat, lng);
-  }
+  function googleUrl(location){
+    return locationUrl + location;
+  };
+
+  function google(location){
+    var location = $('#location').text();
+    var jsonGoogle = {
+      url: googleUrl(location),
+      success: googleSuccess,
+      error: errorHandler
+    };
+
+  function googleSuccess(google){
+    console.log(google);
+    var googleData = {
+      lat: google.results[0].geometry.location.lat,
+      lon: google.results[0].geometry.location.lng,
+    };
+  };
 
   function buildUrl(lat, lon){
     // return "https://api.forecast.io/forecast/099641b3c36867977fb67a197504e301/37.8267,-122.423"
     return baseUrl + apiKey+'/'+lat+','+lon;
-  }
+  };
 
-  function successHandler(data){
-    weatherData = data;
-    $('#output').text(JSON.stringify(data));
-    console.log(data);
-  }
+  // function successHandler(data){
+  //   weatherData = data;
+  //   $('#output').text(JSON.stringify(data));
+  //   console.log(data);
+  // }
 
   function errorHandler(error){
     console.log(error);
-  }
+  };
   function showInfo(){
-    var lat = google(lat);
-    var lon = google(lng);
+    var lat = googleSuccess(lat);
+    var lon = googleSuccess(lon);
     var ajaxOptions = {
       url: buildUrl(lat,lon),
       dataType: 'jsonp',
@@ -42,13 +59,11 @@ $(document).ready(function(){
     var source = $('#info').html();
     var template = Handlebars.compile(source);
     var extractedData = {
-      latitude: hammer.latitude,
-      longitude: hammer.longitude,
-      icon: hammer.currently.icon || 'clear-night',
+      temperature: hammer.currently.temperature,
       summary: hammer.currently.summary,
       time: hammer.currently.time
     };
     var html = template(extractedData);
     $('#test-output').html(html);
-  }
+  };
 });
