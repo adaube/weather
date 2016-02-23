341fd5b1c1c6e2805e2f4b8f2b0d0336a0c114ee
adaube <adam_daube@yahoo.com>
2016-02-22 20:59:02 -0500

    centered content

--- public/js/app.js
+++ public/js/app.js
@@ -31,22 +31,13 @@ $(document).ready(function(){
   };
 
   function buildUrl(lat, lon){
-    // return "https://api.forecast.io/forecast/099641b3c36867977fb67a197504e301/37.8267,-122.423"
     return baseUrl + apiKey+'/'+lat+','+lon;
   };
-
-  // function successHandler(data){
-  //   weatherData = data;
-  //   $('#output').text(JSON.stringify(data));
-  //   console.log(data);
-  // }
 
   function errorHandler(error){
     console.log(error);
   };
   function showInfo(lat, lon){
-    //var lat = googleSuccess(lat);
-    //var lon = googleSuccess(lon);
     var ajaxOptions = {
       url: buildUrl(lat,lon),
       dataType: 'jsonp',
@@ -62,10 +53,14 @@ $(document).ready(function(){
     console.log(hammer);
     var source = $('#info').html();
     var template = Handlebars.compile(source);
+    var w = hammer.currently;
     var extractedData = {
-      temperature: hammer.currently.temperature,
-      summary: hammer.currently.summary,
-      time: hammer.currently.time
+      temperature: w.temperature,
+      icon: w.icon,
+      summary: w.summary,
+      time: w.time,
+      btnLink: "javascript:history.go(0)",
+      btnText: "Click to Reload",
     };
     var html = template(extractedData);
     $('#test-output').html(html);
